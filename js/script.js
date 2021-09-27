ad8e10872a099a082c9eebf76a940aad99694161
Swapnil Narhire <swapnilnarhire@gmail.com>
2021-09-28 00:02:20 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 23, 2020 12:11:00").getTime();
+var countDownDate = new Date("Nov 16, 2021 18:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
